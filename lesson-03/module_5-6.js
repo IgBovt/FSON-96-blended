1af530c35657d0f4da0e5072aa41ac9fdcb82510
Myronenlo <55333290+SergeyMyronenko@users.noreply.github.com>
2023-11-18 17:23:37 +0200

    update task

--- lesson-03/module_5-6.js
+++ lesson-03/module_5-6.js
@@ -151,57 +151,95 @@
 //TODO:=========task-11=================
 // У вас є масив чисел, і вам потрібно знайти середнє значення цих чисел. Напишіть функцію, яка приймає масив чисел і повертає середнє значення.
 
-const arr = [1, 2, 3, 4, 5];
+// const arr = [1, 2, 3, 4, 5];
 
-const averageValue = (array) => array.reduce((acc, number) => acc + number / array.length ,0)
+// const averageValue = (array) => array.reduce((acc, number) => acc + number / array.length ,0)
 
-const res = averageValue(arr);
-console.log(res);
+// const res = averageValue(arr);
+// console.log(res);
 
 // ! this
 //TODO:=========task-01=================
 // Створити об'єкт, який представляє користувача. У об'єкті повинні бути наступні поля: ім'я, прізвище, вік, email та метод, який виводить повну інформацію про користувача.
 
+// const user = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 30,
+//   email: "johndoe@example.com",
+
+//   info() {
+//     console.log(this);
+//   },
+
+//   modify(key, value) {
+//     if (key !== "lastName" && key !== "firstName") {
+//       return alert("Key is not valid!");
+//     }
+//     if (value[0] === value[0].toUpperCase() && value.length >= 3) {
+//       this[key] = value;
+//     }
+//   },
+// };
+
+// const newUser = Object.create(user);
+// newUser.firstName = "Sergiy";
+
+// user.info();
+
+// newUser.info();
+
+// console.log(newUser.lastName);
+
 //TODO:=========task-02=================
 // Для обьекта "user", написати метод для зміни ім'я або прізвища(змінити можна лише якесь з цих полів, змінювати або додавати нові потрібно заборонити) з перевіркою на валідність даних(Перша літера має бути у верхньому реєстрі, довжина слова не менше 3 літер)
 
-// user.modify('firstName', 'Vlad');
+// user.modify("firstName", "Vlad");
+// console.log(user);
 
 //TODO:=========task-03=================
 // Створити глобальну функцію для обьекта "user", що може додавати властивості об'єкту, в контексті якого вона буде викликана. Додати цю функцію як метод об'єкта user, та додати за допомогою неї властивість friends із значенням:
 
-// user.addProperty('friends', [
+// const addProperty = function (key, value) {
+//   this[key] = value;
+// };
+// user.addProperty = addProperty;
+// // newUser.addProperty = addProperty;
+
+// user.addProperty("friends", [
 //   {
-//     firstName: 'Mary',
-//     lastName: 'Smith',
+//     firstName: "Mary",
+//     lastName: "Smith",
 //     age: 32,
-//     email: 'marysmith@hotmail.com',
+//     email: "marysmith@hotmail.com",
 //   },
 //   {
-//     firstName: 'Alex',
-//     lastName: 'Johnson',
+//     firstName: "Alex",
+//     lastName: "Johnson",
 //     age: 45,
-//     email: 'alex.johnson@yahoo.com',
+//     email: "alex.johnson@yahoo.com",
 //   },
 //   {
-//     firstName: 'Emily',
-//     lastName: 'Davis',
+//     firstName: "Emily",
+//     lastName: "Davis",
 //     age: 19,
-//     email: 'emilydavis@gmail.com',
+//     email: "emilydavis@gmail.com",
 //   },
 // ]);
-
-// console.log(user);
+// newUser.addProperty("lastName", "Myr");
+// // console.log(newUser);
 
 //TODO:=========task-04=================
 //  Викликати метод user.info() таким чином, щоб він вивів результатом  ({name: 'Bob', lactName: 'Lasso',age: 50, email: 'BodLasso@gmail.com'})
 
 // const obj = {
-//   name: 'Bob',
-//   lastName: 'Lasso',
+//   name: "Bob",
+//   lastName: "Lasso",
 //   age: 50,
-//   email: 'BodLasso@gmail.com',
+//   email: "BodLasso@gmail.com",
 // };
+
+// user.info.call(obj);
 
 //TODO:=========task-05=================
 //  Викличте функцію showFullName у контексті об'єкта user
@@ -221,9 +259,9 @@ console.log(res);
 //   callback();
 // }
 
-// callAction(product.showPrice);
+// callAction(product.showPrice.bind(product));
 
-//! class
+// ! class
 //TODO:=================task-01=============================
 // Завдання полягає у створенні програми, що дозволяє керувати замовленнями в ресторані.
 
