922b9de59aff5725ca846c7db737705ae7e238ae
SergGrishkov <serg.grishkov@gmail.com>
2023-12-16 15:52:45 +0200

    Added module 08 task 2

--- lesson-05/module-08/js/app.js
+++ lesson-05/module-08/js/app.js
@@ -24,4 +24,39 @@ function handleClick(event) {
  * Використовуй поширення подій.Додавай клас`active` на поточну сторінку. Створи допоміжну функцію`updateActivePage`, яка повинна обновляти активний клас.
  * Функція`handleClick`повинна викликатися коли відбувається клік на елементі`pagination`.
  */
-console.log("Hello");
+import { listContainerEl } from "./refs.js";
+
+listContainerEl.addEventListener("click", updateActivePage);
+
+function updateActivePage(event) {
+  if (event.target.nodeName !== "LI") return;
+  let currentButton = event.target;
+  let activeButton = listContainerEl.querySelector(".active");
+
+  switch (event.target.dataset.type) {
+    case "prev":
+      const prevBthPage = +activeButton.dataset.page - 1;
+      const prevBtn = listContainerEl.querySelector(
+        `[data-page='${prevBthPage}']`
+      );
+      if (prevBtn) {
+        prevBtn.classList.add("active");
+        activeButton.classList.remove("active");
+      }
+      break;
+    case "page":
+      currentButton.classList.add("active");
+      activeButton.classList.remove("active");
+      break;
+    case "next":
+      const nextBthPage = +activeButton.dataset.page + 1;
+      const nextBtn = listContainerEl.querySelector(
+        `[data-page='${nextBthPage}']`
+      );
+      if (nextBtn) {
+        nextBtn.classList.add("active");
+        activeButton.classList.remove("active");
+      }
+      break;
+  }
+}
